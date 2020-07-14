0a237e1e85165d9a03e166988cd7a9a5e87d0807
Kelsey Creehan <kelseycreehan@gmail.com>
2020-07-14 01:17:59 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,5 +1,4 @@
 def find_element_index(array, value_to_find)
   array.length.times do |index|
-    array.include?(value_to_find)
-    if true return value_to_find
+    puts array[count] == value_to_find
 end
